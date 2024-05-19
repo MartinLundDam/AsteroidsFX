640bf5718a881f643873567b42233abfd27e4a00
MartinLundDam <martinlunddam@hotmail.dk>
2024-05-19 13:03:45 +0200

    module-info

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -2,15 +2,18 @@ module Core {
     requires Common;
     requires CommonBullet;    
     requires javafx.graphics;
+    //requires spring
     requires spring.core;
     requires spring.context;
     requires spring.beans;
-    //requires spring.core; //requires spring
+
     exports dk.sdu.mmmi.cbse.main; //export main to make ModuleConfig see the interfaces
     opens dk.sdu.mmmi.cbse.main to javafx.graphics,spring.core;
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
     uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
     uses dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
 }
+
+
 
 
